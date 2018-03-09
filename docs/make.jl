e0f6179d4137061c13fa47706b2b9fa4eeefe700
Eeshan Gupta <eeshan9815@gmail.com>
2018-03-09 15:10:18 +0530

    testing

--- docs/make.jl
+++ docs/make.jl
@@ -16,7 +16,7 @@ makedocs(
 )
 
 deploydocs(
-    repo   = "github.com/JuliaIntervals/IntervalArithmetic.jl.git",
+    repo   = "github.com/eeshan9815/IntervalArithmetic.jl.git",
     target = "build",
     julia  = "0.6",
     osname = "linux",
